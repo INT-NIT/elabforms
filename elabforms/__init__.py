31c7868d8ca935211133b5317206f4f0b707dda0
sifaoufatai <sifaoufatai@gmail.com>
2025-06-19 17:58:36 +0200

    update readme

--- elabforms/__init__.py
+++ elabforms/__init__.py
@@ -7,3 +7,11 @@ from . import generate_templates
 from .template_builder import TemplateBuilder
 from .template_part import TemplatePart
 from .template import Template
+
+__all__ = [
+    "generate_templates",
+    "TemplateBuilder",
+    "TemplatePart",
+    "Template",
+]
+
