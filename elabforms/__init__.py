7d8921ae72b0c1a1645bcb510e09818316038706
sifaoufatai <sifaoufatai@gmail.com>
2025-06-19 17:31:10 +0200

    sphyx doc

---
+++ elabforms/__init__.py
@@ -0,0 +1,9 @@
+"""
+A set of tools to create and manage standardized forms for eLabFTW
+
+"""
+
+from . import generate_templates
+from .template_builder import TemplateBuilder
+from .template_part import TemplatePart
+from .template import Template
