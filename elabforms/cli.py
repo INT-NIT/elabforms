2dded4389f9680b309f2c0daf3e0683d4ba4015a
sifaoufatai <sifaoufatai@gmail.com>
2025-08-06 15:35:11 +0200

    upload package on pypi ( eform VERSION 0.1)

--- elabforms/cli.py
+++ elabforms/cli.py
@@ -2,18 +2,23 @@ import sys
 from .template_builder import TemplateBuilder
 import argparse
 
+
 def main(template_parts_list_file, output_template_file):
+
     """
     Main function to execute the template generation.
     """
     try:
         TemplateBuilder(template_parts_list_file, output_template_file)
-        print(f"Template successfully generated and saved to {output_template_file}")
+        print(f"Template successfully generated and saved to "
+              f"{output_template_file}")
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
 
 def cli():
+
+
     """
     Command-line interface using argparse.
     """
@@ -21,11 +26,13 @@ def cli():
         description="Generate a JSON template from a CSV list of parts."
     )
     argparser.add_argument(
-        "-l", "--template_parts_list", dest="template_parts_list_file", required=True, type=str,
+        "-l", "--template_parts_list", dest="template_parts_list_file",
+        required=True, type=str,
         help="Path to the CSV file listing the parts of the template"
     )
     argparser.add_argument(
-        "-o", "--output", dest="output_template_file", required=True, type=str,
+        "-o", "--output", dest="output_template_file",
+        required=True, type=str,
         help="Path to the output JSON template file"
     )
     args = argparser.parse_args()
